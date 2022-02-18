399f369275b3df1372efa632bfbc63450938b6ff
stef531 <stefan.willmann@skillworks.de>
2022-02-18 12:41:40 +0100

    Initial Jenkins pipeline

---
+++ calcu/src/main/java/com/example/calcu/CalcuApplication.java
@@ -0,0 +1,13 @@
+package com.example.calcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcuApplication.class, args);
+    }
+
+}
